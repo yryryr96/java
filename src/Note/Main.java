01fc91fead14d13e14450280caa7ee05ec6cf15d
정영록 <s9285s@naver.com>
2023-04-12 23:31:19 +0900

    java

--- src/Note/Main.java
+++ src/Note/Main.java
@@ -1,11 +1,47 @@
 import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+public class Main {
 
-class Main {
+    static boolean[][] graph;
+    static int min=64;
     public static void main(String[] args) throws IOException {
-        System.setIn(new FileInputStream("src/input.in"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] s = br.readLine().split(" ");
-        System.out.println(Integer.parseInt(s[0]) -  Integer.parseInt(s[1]));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        graph = new boolean[n][m];
 
+        for (int i = 0; i <n ; i++) {
+            String str = br.readLine();
+            for (int j = 0; j <m ; j++) {
+                if (str.charAt(j) == 'W') graph[i][j] = true;
+                else graph[i][j] = false;
+            }
+        }
+
+        for (int i = 0; i <n-7 ; i++) {
+            for (int j = 0; j <m-7 ; j++) {
+                find(i,j);
+            }
+        }
+        System.out.println(min);
+    }
+
+    public static void find(int i, int j){
+        int ei = i+8;
+        int ej = j+8;
+        int cnt = 0;
+
+        boolean TF = !graph[i][j];
+        for (int k = i; k <ei ; k++) {
+            for (int l = j; l <ej ; l++) {
+                if (graph[k][l] == TF) cnt += 1;
+                TF = !TF;
+            }
+            TF=!TF;
+        }
+        cnt = Math.min(cnt,64-cnt);
+        min = Math.min(min,cnt);
     }
 }
