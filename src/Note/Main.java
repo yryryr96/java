851fafdb7dbb56e13699442450a97b9d497a230f
정영록 <s9285s@naver.com>
2023-04-17 00:45:22 +0900

    java

--- src/Note/Main.java
+++ src/Note/Main.java
@@ -1,28 +1,47 @@
 import java.io.*;
-import java.sql.Array;
 import java.util.*;
 
-public class Main {
-    static long A,B,ans;
-    static boolean temp;
-    public static void main(String[] args) throws Exception {
+public class Main{
+    static boolean[] visited;
+    static int[] arr;
+    static int[] lst;
+    static int n;
+    static int sum ;
+        public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        sum = 0;
+        visited = new boolean[n];
+        lst = new int[n];
+        arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        A = Long.parseLong(st.nextToken());
-        B = Long.parseLong(st.nextToken());
-        dfs(A,1);
-        if (temp) System.out.println(ans);
-        else System.out.println(-1);
+        for (int i = 0; i <n ; i++) {
+            lst[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dfs(0,0);
+        System.out.println(sum);
+        
     }
 
-    public static void dfs(long num, int cnt){
-        if (num > B ) return;
-        if (num==B){
-            ans = cnt;
-            temp = true;
+    public static void dfs(int idx,int depth){
+        if (depth == n){
+            int s = 0;
+            for (int i = 0; i <n-1; i++) {
+                s += Math.abs(arr[i]-arr[i+1]);
+            }
+            if (s > sum) sum = s;
             return;
         }
-        dfs(num*2,cnt+1);
-        dfs(num*10+1,cnt+1);
+
+        for (int i = 0; i < n ; i++) {
+            if (!visited[i]){
+                visited[i] = true;
+                arr[depth] = lst[i];
+                dfs(i,depth+1);
+                visited[i] = false;
+            }
+        }
     }
+
 }
