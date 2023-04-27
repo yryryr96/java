65844a58d8ba23df1361ff936c1a2e4f167e7ebc
정영록 <s9285s@naver.com>
2023-04-27 23:44:39 +0900

    Java

--- src/Note/Main.java
+++ src/Note/Main.java
@@ -2,30 +2,50 @@ import java.io.*;
 import java.util.*;
 
 public class Main{
+    static StringTokenizer st;
+    static char[][] graph;
+    static Deque<int[]> q;
+    static int[][] visited;
+    static int n;
+    static int m;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        int n = Integer.parseInt(br.readLine());
-        Integer[] a = new Integer[n];
-        Integer[] b = new Integer[n];
-
         st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        q = new ArrayDeque<>();
+        visited = new int[n][m];
+        graph = new char[n][m];
+
         for (int i = 0; i <n ; i++) {
-            a[i] = Integer.parseInt(st.nextToken());
+            String hang = br.readLine();
+            graph[i] = hang.toCharArray();
         }
 
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i <n ; i++) {
-            b[i] = Integer.parseInt(st.nextToken());
-        }
+        bfs();
+        System.out.println(visited[n-1][m-1]);
+    }
 
-        Arrays.sort(a);
-        Arrays.sort(b,Collections.reverseOrder());
-        int ans = 0;
-        for (int i = 0; i <n ; i++) {
-            ans += (a[i]*b[i]);
+    public static void bfs() {
+        int[] di = {0,0,1,-1};
+        int[] dj = {1,-1,0,0};
+        visited[0][0] = 1;
+        q.add(new int[] {0,0});
+        while(!q.isEmpty()) {
+            int[] now = q.removeFirst();
+            int i = now[0];
+            int j = now[1];
+            if (i==n-1 && j==m-1) return;
+
+            for (int k = 0; k <4 ; k++) {
+                int ni = i +di[k];
+                int nj = j +dj[k];
+                if (0<=ni && ni<n && 0<=nj && nj<m && visited[ni][nj] == 0 && graph[ni][nj] == '1' ) {
+                    q.add(new int[] {ni,nj});
+                    visited[ni][nj] = visited[i][j] + 1;
+                }
+            }
+
         }
-        System.out.println(ans);
-        
     }
 }
